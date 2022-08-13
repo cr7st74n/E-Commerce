fd33dd17828dac5689c7b0426918a21b63a633a9
cr7st74n <cris-082011@hotmail.com>
2022-08-12 20:44:25 -0500

    models added

--- models/Tag.js
+++ models/Tag.js
@@ -4,9 +4,16 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
+Tag.init({
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey:true,
+      autoIncrement: true
+    },
+    tag_name:{
+      type:DataTypes.STRING
+    }
   },
   {
     sequelize,
